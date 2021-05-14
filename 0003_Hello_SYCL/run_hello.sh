55e9959fed828100629c49c0ca634393a3175705
James Reinders <james.r.reinders@intel.com>
2021-05-14 12:19:59 -0700

    minor fix up of blog number in run script

--- 0003_Hello_SYCL/run_hello.sh
+++ 0003_Hello_SYCL/run_hello.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-/bin/echo "##" $(whoami) is compiling xpublog 1 -- Hello SYCL - hello.cpp
+/bin/echo "##" $(whoami) is compiling xpublog -- Hello SYCL - hello.cpp
 make
-/bin/echo "##" $(whoami) is running xpublog 1 -- Hello SYCL - hello.cpp
+/bin/echo "##" $(whoami) is running xpublog -- Hello SYCL - hello.cpp
 make run
 
