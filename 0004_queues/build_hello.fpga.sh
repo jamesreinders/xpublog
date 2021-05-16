1fd900833793673e0cc961b9bb1376516b652e78
James Reinders <james.r.reinders@intel.com>
2021-05-15 18:33:14 -0700

    clean up FPGA build then run

--- 0004_queues/build_hello.fpga.sh
+++ 0004_queues/build_hello.fpga.sh
@@ -2,6 +2,4 @@
 . /opt/intel/inteloneapi/setvars.sh
 /bin/echo "##" $(whoami) is compiling xpublog 4 -- Hello SYCL for FPGA - hello.cpp
 make build_fpga
-/bin/echo "##" $(whoami) is running xpublog 4 -- Hello SYCL for FPGA - hello.cpp
-make run_fpga
 
