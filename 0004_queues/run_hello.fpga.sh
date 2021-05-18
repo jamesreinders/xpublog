949193ac37940d6ed11d15b0209e025180428651
James Reinders <james.r.reinders@intel.com>
2021-05-18 10:43:40 -0700

    cleaning up FPGA run

--- 0004_queues/run_hello.fpga.sh
+++ 0004_queues/run_hello.fpga.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 . /opt/intel/inteloneapi/setvars.sh
 /bin/echo "##" $(whoami) is running xpublog 4 -- Hello SYCL for FPGA - hello.cpp
+echo "---aocl diagnose >>>"
+aocl diagnose
+echo "<<< aocl diagnose---"
+find / -name "libopae-c.so" 2>/dev/null
 make run_fpga
 
